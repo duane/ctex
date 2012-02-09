f0cf85fba66a7452b67c61123bb365dc1d9efdea
Duane R. Bailey <bailey.d.r@gmail.com>
2012-02-09 16:46:19 -0500

    Added Diag classes for Diagnostic information.
    Added BlameDiag for blaming diagnostics on input file errors.
    Added GenericDiag for generic diagnostics, e.g. errors opening/writing files.

---
+++ include/Diag/Diag.h
@@ -0,0 +1,99 @@
+#ifndef __INCLUDE_DIAG_DIAG_H__
+#define __INCLUDE_DIAG_DIAG_H__
+
+#include <IO/BlameSource.h>
+
+namespace tex {
+
+enum {
+  DIAG_OK = 0,
+  DIAG_FILE_OPEN_ERR,
+  DIAG_FILE_WRITE_ERR,
+};
+
+class Diag {
+public:
+  virtual char *string() const = 0;
+  virtual int errno() const = 0;
+  virtual void print() = 0;
+  
+  virtual operator bool() const {
+    return errno() != DIAG_OK;
+  }
+  
+  virtual BlameSource source() const = 0;
+};
+
+class GenericDiag : public Diag {
+private:
+  const char *message;
+  int err;
+  BlameSource diag_source;
+  char *str;
+public:
+  GenericDiag(const char *msg, int errno, BlameSource source);
+  char *string(void) const {
+    assert(str && "Attempted to return NULL diag string.");
+    return str;
+  }
+  
+  int errno(void) const {
+    return err;
+  }
+  
+  BlameSource source(void) const {
+    return diag_source;
+  }
+  
+  void print(void);
+};
+
+class BlameSourceDiag : public Diag {
+private:
+  const char *message;
+  int err;
+  char *str;
+  BlameSource text_source, diag_source;
+public:
+  char *string(void) const {
+    return str;
+  }
+  
+  int errno(void) const {
+    return err;
+  }
+  
+  BlameSource source(void) const {
+    return diag_source;
+  }
+  
+  BlameSource input_source(void) const {
+    return text_source;
+  }
+  
+  void print();
+  BlameSourceDiag(const char *msg, int err, BlameSource &diag_source, BlameSource &input_source);
+};
+
+class OKDiag : public Diag {
+public:
+  OKDiag(void) {}
+  
+  char *string(void) const {
+    return (char*)"";
+  }
+  
+  int errno(void) const {
+    return DIAG_OK;
+  }
+  
+  BlameSource source(void) const {
+    return BlameSource();
+  }
+  
+  void print(void);
+};
+
+}  // namespace tex
+
+#endif  // __INCLUDE_DIAG_DIAG_H__
---
+++ libs/Diag/Diag.cc
@@ -0,0 +1,53 @@
+#include <cassert>
+
+#include <Diag/Diag.h>
+
+
+using namespace tex;
+
+GenericDiag::GenericDiag(const char *msg, int errno, BlameSource source) {
+  message = msg;
+  err = errno;
+  diag_source = source;
+  
+  assert(message && "Attempted to create a GenericDiag with NULL message.");
+  
+  std::string string = std::string(diag_source.string());
+  string += std::string(": ");
+  string += std::string(message, 2048);
+  size_t str_len = string.length() + 1;
+  str = new char[str_len];
+  memcpy(str, string.c_str(), str_len);
+}
+
+void GenericDiag::print(void) {
+  puts(str);
+}
+
+
+BlameSourceDiag::BlameSourceDiag(const char *msg, int error, BlameSource &generationSource, BlameSource &errorSource) {
+  assert(msg && "Got passed null diagnostic message.");
+
+  message = msg;
+  err = error;
+  diag_source = generationSource;
+  text_source = errorSource;
+  
+  std::string string = std::string("Diagnostic generated from ");
+  string += std::string(diag_source.string());
+  string += std::string(":\n");
+  string += std::string(text_source.string());
+  string += std::string(": ");
+  string += std::string(msg, 2048);
+  
+  size_t str_len = string.length() + 1;
+  str = new char[str_len];
+  memcpy(str, string.c_str(), str_len);
+}
+
+void BlameSourceDiag::print() {
+  puts(message);
+}
+
+void OKDiag::print() { 
+}
